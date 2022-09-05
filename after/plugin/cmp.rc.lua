ae1e9a1d904838660a257796462f6421d886fcae
Ben Borla <ben.borla@gamesysgroup.com>
2022-09-06 02:13:03 +0800

    added lsp

--- after/plugin/cmp.rc.lua
+++ after/plugin/cmp.rc.lua
@@ -4,7 +4,7 @@ if (not status) then return end
 local snip_status_ok, luasnip = pcall(require, "luasnip")
 if (not snip_status_ok) then return end
 
-require("luasnip/loaders/from_vscode").lazy_load()
+require("luasnip.loaders.from_vscode").lazy_load()
 
 local check_backspace = function()
   local col = vim.fn.col "." - 1
@@ -90,8 +90,8 @@ cmp.setup({
     }),
   },
   sources = cmp.config.sources({
-    { name  = 'luasnip' },
     { name  = 'nvim_lsp' },
+    { name  = 'luasnip' },
     { name = 'buffer' },
     { name = 'path' }
   }),
@@ -102,7 +102,8 @@ cmp.setup({
       vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
       -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       vim_item.menu = ({
-        luasnip = "[luasnip]",
+        nvim_lsp = "[LSP]",
+        luasnip = "[Snippet]",
         buffer = "[Buffer]",
         path = "[Path]",
       })[entry.source.name]
